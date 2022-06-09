a11c06b600a3c0165f7b71207d508850b9496169
quynhpn2610 <quynhphuongnguyen.2610@gmail.com>
2022-06-09 19:16:53 +0700

    written 05

--- src/TwoDArrays/PrimesProductDiagonal2_05.java
+++ src/TwoDArrays/PrimesProductDiagonal2_05.java
@@ -1,4 +1,42 @@
 package TwoDArrays;
 
+import java.util.Scanner;
+
 public class PrimesProductDiagonal2_05 {
+    private static boolean isPrime(int n){
+        if (n < 2) return false;
+
+        for (int i = 2; i <= (double)(Math.sqrt(n)); i++) {
+            if (n % i == 0) return false;
+        }
+
+        return true;
+    }
+    public static int productDiagonalPrime(int[][] a){
+        int product = 1;
+        for (int i = 0; i < a.length; i++) {
+            if(isPrime(a[i][a.length-1-i])) {
+                product *= a[i][a.length-1-i];
+            }
+        }
+        return product;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+
+        int[][] a = new int[m][m];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < m; j++) {
+                a[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println(productDiagonalPrime(a));
+
+
+        sc.close();
+    }
 }
