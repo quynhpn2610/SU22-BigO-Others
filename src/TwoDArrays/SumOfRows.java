bad24dfecd5413c6f2914d5696f1e821154bfb79
quynhpn2610 <quynhphuongnguyen.2610@gmail.com>
2022-06-09 15:34:53 +0700

    sum of rows

---
+++ src/TwoDArrays/SumOfRows.java
@@ -0,0 +1,34 @@
+package TwoDArrays;
+
+import java.util.Scanner;
+
+public class SumOfRows {
+    private static void sumCalc(int[][] a){
+        int sum = 0;
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[0].length; j++) {
+                sum += a[i][j];
+            }
+            System.out.println(i + ": " + sum);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+        int[][] a = new int[m][n];
+
+        for (int i = 0; i < m; i++){
+            for (int j = 0; j < n; j++) {
+                a[i][j] = sc.nextInt();
+            }
+
+        }
+
+        sumCalc(a);
+        sc.close();
+
+
+
+    }
+}
