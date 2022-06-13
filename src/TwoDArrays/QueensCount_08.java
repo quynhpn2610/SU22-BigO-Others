f8f8c0fcd621906eb1e350dbd5fdbf8dfc9a4bb3
quynhpn2610 <quynhphuongnguyen.2610@gmail.com>
2022-06-13 11:03:52 +0700

    Done with matrix

--- src/TwoDArrays/QueensCount_08.java
+++ src/TwoDArrays/QueensCount_08.java
@@ -1,72 +1,61 @@
-// find max of row & col & 2 diagonals passing through it
 package TwoDArrays;
 
-import java.util.Scanner;
+import java.util.*;
+class QueensCount_08{
 
-public class QueensCount_08 {
-    private static int countQueen(int[][] a){
-        int queenCount = 0;
-        int nRow = a.length;
-        int nCol = a[0].length;
-        boolean[][] maxEachRow = new boolean[nRow][nCol];
-        boolean[][] maxEachCol = new boolean[nRow][nCol];
-        boolean[][] maxRowCol = new boolean[nRow][nCol];
-
-        // Loop qua tung hang
-        for (int i = 0; i < a.length; i++) {
-            int maxThisRow = Integer.MIN_VALUE;
-            int maxCol = 0;
-            int maxRow = 0;
-            for (int j = 0; j < a[i].length; j++) {
-                if (a[i][j] > maxThisRow) {
-                    maxThisRow = a[i][j];
-                    maxRow = i;
-                    maxCol = j;
-                }
+    private static boolean isMaxOnThisDirection(int[][] a, int row, int col, int dxx, int dyy)
+    {
+        // duyệt qua các ô trên hướng này gọi là ô a[x][y]
+        // a[row][col] < a[x][y] -> return false
+        int x = row+dxx;
+        int y = col+dyy;
+        // 0 <= x < m, 0 <= y < n
+        int m = a.length;
+        int n = a[0].length;
+        while (x >= 0 && x < m && y >= 0 && y < n){
+            if (a[row][col] < a[x][y]){
+                return false;
             }
-            maxEachRow[maxRow][maxCol] = true;
+            x += dxx;
+            y += dyy;
         }
-        // Loop qua tung cot
-        for (int j = 0; j < a[0].length; j++) {
-            int maxThisCol = Integer.MAX_VALUE;
-            int maxCol = 0;
-            int maxRow = 0;
-            for (int i = 0; i < a.length; i++) {
-                if(a[i][j] > maxThisCol){
-                    maxThisCol = a[i][j];
-                    maxCol = j;
-                    maxRow = i;
-                }
-            }
-        }
-        // Loop qua 2 matrix de tim matching elements (both true)
-        for (int i = 0; i < maxEachRow.length; i++) {
-            for (int j = 0; j < maxEachRow[i].length; j++) {
-                if (maxEachRow[i][j] & maxEachCol[i][j]) {
-                    queenCount ++;
-                    maxRowCol[i][j] = maxEachRow[i][j];
-                }
-            }
-        }
-
-
-
-        return queenCount;
-
+        return true;
     }
+    private static boolean isQueen(int[][] a, int row, int col){
 
+        int[] dx = {-1, -1, -1, 0, 0, 1, 1, 1};
+        int[] dy = {-1, 0, 1, -1, 1, -1, 0, 1};
+        // lớn nhất trên 8 hướng ?
+        for (int i = 0; i<8; i++){
+            // lớn nhất trên dx[i], dy[i] ?
+            if (isMaxOnThisDirection(a, row, col, dx[i], dy[i]) == false)
+                return false;
+            // xử lý xong hướng dx[i],dy[i]
+        }
+        return true;
+    }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int m = sc.nextInt();
+        int count = 0;
 
         int[][] a = new int[m][m];
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < m; j++) {
                 a[i][j] = sc.nextInt();
+
             }
         }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < m; j++) {
+                if (isQueen(a, i, j)) count ++;
+            }
+        }
+        System.out.print(count);
         sc.close();
     }
+
 }
