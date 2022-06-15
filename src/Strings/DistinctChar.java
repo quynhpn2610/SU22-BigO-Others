a71c47572a42dc526997d53182c2405960649173
quynhpn2610 <quynhphuongnguyen.2610@gmail.com>
2022-06-15 20:45:50 +0700

    distinct char

--- src/Strings/DistinctChar.java
+++ src/Strings/DistinctChar.java
@@ -1,4 +1,29 @@
 package Strings;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Scanner;
+
 public class DistinctChar {
+    private static int countDistinctChar(String s){
+        int count = 0;
+        char[] sArr = s.toCharArray();
+        HashSet<Character> mySet = new HashSet<Character>();
+        for (char ch:
+             sArr) {
+            if (mySet.contains(ch)){
+                continue;
+            }
+            mySet.add(ch);
+        }
+        count = mySet.size();
+        return count;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        System.out.println(countDistinctChar(s));
+        sc.close();
+    }
 }
