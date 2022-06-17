6693881cdc24a370d85be6226281e763bf9b462d
quynhpn2610 <quynhphuongnguyen.2610@gmail.com>
2022-06-17 23:45:04 +0700

    @ton.tun passed

--- src/MidTerm/ValidEmail.java
+++ src/MidTerm/ValidEmail.java
@@ -13,7 +13,7 @@ public class ValidEmail {
 
         // check if either local or domain is null
         for (int i = 0; i < sArr.length; i++) {
-            if (sArr[i] == null){
+            if (sArr[i].length() == 0){
                 return false;
             }
         }
@@ -28,25 +28,47 @@ public class ValidEmail {
 
         // Check domain
         char[] sArr1 = sArr[1].toCharArray();
+
+            // check if two consecutive dots or only 1 dots
+        int count = 0;
         for (int i = 0; i < sArr1.length-1; i++){
+            if (sArr1[i] == '.'){
+                count++;
+            }
             if (sArr1[i] == '.' && sArr1[i+1] == '.'){
                 return false;
             }
         }
+        if (sArr1[sArr1.length-1] == '.'){
+            count++;
+        }
+        if (count==0) return false;
+
+
+            // check if other characters is valid
         for (char ch:
              sArr1) {
-            if (!((int) ch >= 65 && (int) ch <= 90) && !((int) ch >= 97 && (int) ch <= 122) && ch != '.'){
+            boolean isNotDot = (ch != '.');
+            boolean isNotLowercase = !((int) ch >= 65 && (int) ch <= 90);
+            boolean isNotUppercase = !((int) ch >= 97 && (int) ch <= 122);
+            if (isNotDot && isNotLowercase && isNotUppercase){
                 return false;
             }
         }
 
         return true;
     }
+
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
-        if (isValidEmail(s)) System.out.println("VALID");
-        else System.out.println("INVALID");
+        if (isValidEmail(s)){
+            System.out.println("VALID");
+        }
+        else {
+            System.out.println("INVALID");
+        }
         sc.close();
     }
 }
