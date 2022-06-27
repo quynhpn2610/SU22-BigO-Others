1538ce3ce22fa43666b5a0d09011ddf3d6e5ab6c
quynhpn2610 <phuongquynhnguyen.2610@gmail.com>
2022-06-27 16:24:42 +0700

    Insertion sort ascending

--- src/Sorting/Ascending.java
+++ src/Sorting/Ascending.java
@@ -1,20 +1,22 @@
 package Sorting;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Ascending {
-    public static int[] ascInsertionSort(int[] a){
+    public static void ascInsertionSort(int[] a){
         for (int i = 1; i < a.length; i++) {
-            int x = a[i];
-            for (int j = i; j > 0; j--) {
-                if (a[j-1] <= x){
-                    break;
-                }
-                a[j] = a[j-1];
+            int key = a[i];
+            int j = i-1;
+            while (j >= 0 && a[j] > key) //because this is why we need to shift
+                {
+                a[j+1] = a[j];
+                j--;
             }
+            a[j+1] = key;
         }
-        return a;
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
@@ -22,6 +24,9 @@ public class Ascending {
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
-
+        ascInsertionSort(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
     }
 }
